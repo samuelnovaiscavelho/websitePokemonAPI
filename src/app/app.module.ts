d47f771d1354fdf71d9fc27c1e32cb4e6c925cf5
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-09-17 03:16:42 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonService } from './services/pokemon.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    //Chama os componentes para modules (parte de rotas);
+    AppComponent,
+    PokemonListComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PokemonService], //Ao registrar o PokemonService posso vincular a aqualquer componente que estiver declarado aqui no declaration.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
